a4f7f24c8ab2bc4c1c5767b03aa9ae21af3d327f
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-02-24 15:28:51 +0330

    Update: enhance auth middleware to set layout based on user role

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import {defineNuxtRouteMiddleware, useCookie} from "#app";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const user  = useCookie('user');
+    const user = useCookie('user');
 
     if (to.meta.requiresAuth && !user.value) {
         return navigateTo('/login');
@@ -9,6 +9,14 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     if (to.meta.requiresRole && to.meta.requiresRole !== user.value.role) {
         return navigateTo('/403');
+    }
+
+    // set layout
+    to.meta.layout = 'default';
+    switch (user.value.role) {
+        case 'admin':
+            to.meta.layout = 'admin';
+            break;
     }
 
 });
